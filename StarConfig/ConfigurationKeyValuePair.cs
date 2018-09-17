2be296634920ecfc1d690e70d8cdb65189cff241
Erik von Krusenstierna <erik.vk@me.com>
2018-09-18 00:28:30 +0200

    Add project files.

---
+++ StarConfig/ConfigurationKeyValuePair.cs
@@ -0,0 +1,9 @@
+﻿using Dynamit;
+
+namespace StarConfig
+{
+    public class ConfigurationKeyValuePair : DKeyValuePair
+    {
+        public ConfigurationKeyValuePair(DDictionary dict, string key, object value = null) : base(dict, key, value) { }
+    }
+}
---
+++ StarConfigExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+using Starcounter;
+
+namespace StarConfigExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            Handle.GET("/config", () =>
+            {
+                var configuration = Configuration.Current
+                return JsonConvert.SerializeObject(Db.SQ);
+            });
+        }
+    }
+}
